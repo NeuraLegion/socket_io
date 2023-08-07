50722b91ab49586a982d207d241ce59715f8d8e9
Artem Derevnjuk <artem.derevnjuk@neuralegion.com>
2023-08-07 10:57:03 +0400

    feat(socket_io): add support for `NamedTuple`

--- src/socket_io/json_parser.cr
+++ src/socket_io/json_parser.cr
@@ -15,6 +15,14 @@ module SocketIO
       parse(x.to_a)
     end
 
+    def parse(x : NamedTuple)
+      parse(x.to_h)
+    end
+
+    def parse(x : Symbol)
+      parse(x.to_s)
+    end
+
     def parse(x : Array)
       return x.map { |e| JSON::Any.new(parse(e).as(JSON::Any::Type)) }.as(JSON::Any::Type)
     end
@@ -22,6 +30,7 @@ module SocketIO
     def parse(x : Hash)
       h = {} of String => JSON::Any
       x.each_with_object(h) do |(k, v), h|
+        k = k.to_s if k.is_a?(Symbol)
         h[k.as(String)] = JSON::Any.new(parse(v).as(JSON::Any::Type))
       end
       h.as(JSON::Any::Type)
